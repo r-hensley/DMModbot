c620b392b1ec7df3b28b01a08594b40efc0aafcc
Ryry013 <30587035+ryry013@users.noreply.github.com>
2024-10-18 00:12:55 -0700

    Change utils.is_emoji to emoji.is_emoji

--- cogs/utils/helper_functions.py
+++ cogs/utils/helper_functions.py
@@ -24,45 +24,6 @@ here.loop = None
 SP_SERV_ID = 243838819743432704
 JP_SERV_ID = 189571157446492161
 
-_url = re.compile(
-    r"""
-            # protocol identifier
-            (?:https?|ftp)://
-            # user:pass authentication
-            (?:\S+(?::\S*)?@)?
-            (?:
-              # IP address exclusion
-              # private & local networks
-              (?!(?:10|127)(?:\.\d{1,3}){3})
-              (?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})
-              (?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})
-              # IP address dotted notation octets
-              # excludes loopback network 0.0.0.0
-              # excludes reserved space >= 224.0.0.0
-              # excludes network & broacast addresses
-              # (first & last IP address of each class)
-              (?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])
-              (?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}
-              \.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4])
-            |
-              # host name
-              (?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+
-              # domain name
-              (?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*
-              # TLD identifier
-              \.[a-z\u00a1-\uffff]{2,}
-              # TLD may end with dot
-              \.?
-            )
-            # port number
-            (?::\d{2,5})?
-            # resource path
-            (?:[/?#]\S*)?
-        """, re.VERBOSE | re.I)
-
-_emoji = re.compile(r'<a?(:[A-Za-z0-9_]+:|#|@|@&)!?[0-9]{17,20}>')
-
-
 def setup(bot: commands.Bot, loop):
     """This command is run in the setup_hook function in Modbot.py"""
     if here.bot is None:
@@ -283,48 +244,6 @@ async def add_report_to_db(author: discord.User, report_thread: discord.Thread):
 EXEMPTED_BOT_PREFIXES = ['_', ';', '.', ',', '>', '&', 't!', 't@', '$', '!', '?']
 
 
-def is_emoji(char):
-    EMOJI_MAPPING = (
-        # (0x0080, 0x02AF),
-        # (0x0300, 0x03FF),
-        # (0x0600, 0x06FF),
-        # (0x0C00, 0x0C7F),
-        # (0x1DC0, 0x1DFF),
-        # (0x1E00, 0x1EFF),
-        # (0x2000, 0x209F),
-        # (0x20D0, 0x214F),
-        # (0x2190, 0x23FF),
-        # (0x2460, 0x25FF),
-        # (0x2600, 0x27EF),
-        # (0x2900, 0x2935),
-        # (0x2B00, 0x2BFF),
-        # (0x2C60, 0x2C7F),
-        # (0x2E00, 0x2E7F),
-        # (0x3000, 0x303F),
-        (0xA490, 0xA4CF),
-        (0xE000, 0xF8FF),
-        (0xFE00, 0xFE0F),
-        (0xFE30, 0xFE4F),
-        (0x1F000, 0x1F02F),
-        (0x1F0A0, 0x1F0FF),
-        (0x1F100, 0x1F64F),
-        (0x1F680, 0x1F6FF),
-        (0x1F910, 0x1F96B),
-        (0x1F980, 0x1F9E0),
-    )
-    return any(start <= ord(char) <= end for start, end in EMOJI_MAPPING)
-
-
-def rem_emoji_url(msg):
-    if isinstance(msg, discord.Message):
-        msg = msg.content
-    new_msg = _emoji.sub('', _url.sub('', msg))
-    for char in msg:
-        if is_emoji(char):
-            new_msg = new_msg.replace(char, '').replace('  ', '')
-    return new_msg
-
-
 async def wait_for_further_info_from_op(report_entry_message: discord.Message,
                                         desired_chars: int = 150,
                                         ban_appeal: bool = False):
@@ -383,7 +302,7 @@ async def wait_for_further_info_from_op(report_entry_message: discord.Message,
                 continue
 
             # delete URLs from the message
-            candidate_text = rem_emoji_url(response)
+            candidate_text = utils.rem_emoji_url(response)
             
             # check if the text after removing spaces and punctuation no longer has any length
             # use regex
